69f8d5300c160ade7240dfe9963dbdf566a7fe4e
okauppinen <olli.kauppinen@nls.fi>
2021-02-26 10:31:42 +0200

    override layerlist guidedtour message localization

--- bundles/asdi-lang-overrides/resources/locale/fi.js
+++ bundles/asdi-lang-overrides/resources/locale/fi.js
@@ -6,3 +6,14 @@ Oskari.registerLocalization(
         "notLoggedIn": "Kirjautuneena käyttäjänä voit tarkastella ja muokata täällä käyttäjätietojasi sekä omia karttanäkymiäsi ja karttajulkaisujasi."
     }
 }, true);
+
+Oskari.registerLocalization(
+{
+    "lang": "fi",
+    "key": "LayerList",
+    "value": {
+        "guidedTour": {
+            'message': 'Karttatasot-valikosta löydät kaikki karttapalvelussa saatavilla olevat karttatasot.<br/><br/>Järjestä karttatasot joko aiheen tai tiedontuottajan mukaan.<br/><br/>Hae karttatasoja karttatason nimen, tiedontuottajan nimen tai avainsanan perusteella. Löydät uusimmat karttatasot, vektoritasot ja julkaistavissa olevat karttatasot valmiiksi määritellyiltä listoilta.<br/><br/>Avoinna olevat karttatasot voit tarkistaa Valitut tasot -välilehdeltä. Voit raahata karttatasot uuteen järjestykseen, säätää tasojen näkyvyyttä tai vaihtaa esitystyyliä osalla karttatasoista.'
+        }
+    }
+}, true);
--- bundles/asdi-lang-overrides/resources/locale/sv.js
+++ bundles/asdi-lang-overrides/resources/locale/sv.js
@@ -6,3 +6,14 @@ Oskari.registerLocalization(
         "notLoggedIn": "Mina uppgifter innehåller din användarprofil och alla dina sparade kartvyer och inbäddade kartor."
     }
 }, true);
+
+Oskari.registerLocalization(
+{
+    "lang": "sv",
+    "key": "LayerList",
+    "value": {
+        "guidedTour": {
+            'message': 'På Kartlager-listan hittar du alla i karttjänsten tillgängliga kartlager.<br/><br/>Sortera kartlager antingen med tema eller med dataproducent.<br/><br/>Sök kartlager med kartlagernamn, dataproducents namn eller sökord. De nyaste kartlager, kartlager med vektorgrafik eller kartlager som kan publiceras hittar du på färdiga listor.<br/><br/>Bladet Valda kartlager visar vilka kartlager är öppet. Du kan dra dessa kartlager i en annan ordning, justera kartlagrens opacitet eller byta stil på en del kartlager.'
+        }
+    }
+}, true);
