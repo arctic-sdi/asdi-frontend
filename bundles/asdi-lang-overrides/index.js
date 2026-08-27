c633829b4b38052570e11064b4d4da478029325a
Sami Mäkinen <sami.makinen@nls.fi>
2026-08-27 12:49:26 +0300

    Fix name to one used in db

--- bundles/asdi-lang-overrides/index.js
+++ bundles/asdi-lang-overrides/index.js
@@ -1,6 +1,6 @@
 // only act as a reference point to gather localizations
 // register create function for bundleid
-Oskari.bundle('lang-overrides', () => {
+Oskari.bundle('asdi-lang-overrides', () => {
     return {
         start: () => { /* no-op */ },
         stop: () => { /* no-op */ }
